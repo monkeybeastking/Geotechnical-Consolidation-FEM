caf190571678f19146d0861f6140f9c0169cf16d
monkeybeastking <uthmanaziz2001@gmail.com>
2026-01-12 21:32:45 +0000

    message

---
+++ scripts/1D_Terazaghi/1D_analytical.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+H = 5
+n = 100
+nodes = n + 1
+P = 100
+Tx = 60*60*24*150
+time_step = 100
+dt = Tx / time_step
+Cv = 2e-7
+
+time_factor = (Cv * dt) / (H**2) 
+
+def terzaghi(H, Tx, time_step, nodes, Cv):
+    Z = np.linspace(0, H, nodes)                 
+    Cdata = np.zeros((time_step, nodes), dtype=float)
+    T = np.linspace(0, Tx, time_step, dtype=float)
+    N_terms = 1000
+
+    for i in np.arange(nodes):
+        z = Z[i]
+        for j in np.arange(len(T)):
+            t = T[j]
+            S = 0.0
+            for n in range(N_terms):
+                m = (2 * n) + 1
+                S += (1.0 / m) * np.sin((m * np.pi * z) / (2.0 * H)) * np.exp(-((m**2) * (np.pi**2) * Cv * t) / (4.0 * (H**2)))
+            
+            Cn = (4.0 / np.pi) * S
+            Cdata[j, i] = Cn
+    return Cdata
+
+cdata = terzaghi(H, Tx, time_step, nodes, Cv)
+cdata = 1 - cdata
+print(cdata)
+Z = -np.linspace(0, H, nodes)
+
+
+plt.plot(cdata[0, :], Z, label = f"Day 0 (Tv={time_factor:.4f})")
+plt.plot(cdata[int(time_step/3), :],Z, label= f"day 50 (Tv={time_factor:.4f})")
+plt.plot(cdata[int(time_step/3), :],Z, label= f"day 100 (Tv={time_factor:.4f})")
+plt.plot(cdata[(time_step-1), :],Z, label= f"day 150 (Tv={time_factor:.4f})")
+plt.legend()
+plt.xlabel("degree of consolidation")
+plt.ylabel("Depth (m)")
+plt.title("Pore Pressure vs Depth")
+plt.show()
