3bc0d9a029c611f219eeb498a9572ffa18fcdd75
monkeybeastking <uthmanaziz2001@gmail.com>
2026-01-25 10:28:43 +0000

    add basic streamlit dashboard

--- app/app.py
+++ app/app.py
@@ -1,1 +1,13 @@
-import streamlit
+import streamlit as st
+import matplotlib.pyplot as plt
+
+from numpy.random import default_rng as rng
+
+arr = rng(0).normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.title("Plotting using streamlit and matplotlib")
+st.pyplot(fig)
+
+
