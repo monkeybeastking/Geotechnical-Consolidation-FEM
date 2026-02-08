2a2970c5600fb78110f5d6d6a2bf754aebbac3b2
monkeybeastking <uthmanaziz2001@gmail.com>
2026-02-08 21:16:09 +0000

    modulising multi layered model - settlement needs to be done

--- pages/2_1d_multilayer_terazaghi.py
+++ pages/2_1d_multilayer_terazaghi.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 
-from scripts.terazaghi_1d.fea_fenicsx import Get_Terazaghi1D_FEA 
-from scripts.settlements import get_settlement
-from scripts.initial_conditions import Boussinesq_condition
+from scripts.terazaghi_1d_multilayer.mfea_fenics import Get_Terazaghi1dMultilayer_FEA
 
 # setting up Page config for streamlit 
 st.set_page_config(layout ="wide")
 st.title("1D Terazaghi Consolidation Settlement - Multi Layer")
+
+
+
---
+++ scripts/terazaghi_1d_multilayer/mfea_fenics.py
@@ -0,0 +1,243 @@
+from pathlib import Path
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType 
+
+import numpy as np
+from petsc4py import PETSc
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+import ufl
+from dolfinx import fem, io, mesh, plot
+from dolfinx.fem import petsc
+from dolfinx.fem.petsc import LinearProblem
+from dolfinx.mesh import meshtags
+from dolfinx.fem.petsc import (
+    assemble_vector,
+    assemble_matrix,
+    create_vector,
+    apply_lifting,
+    set_bc)
+
+
+# these are made specailly for a FEniCSx Function and not to be used normally
+def Boussinesq(z):  # no input paramters required as this is only recalled under the same paramter each time
+    """
+    Boussinesq initial condition (strip load, centreline) for Terzaghi consolidation.
+    Assumptions:
+    - uniform embankment pressure q
+    - embankment width B
+    - 1D column at centreline
+    - elastic stress distribution used only for initial condition
+    """
+    z = np.maximum(z,1e-12)
+    u = (2.0 * Load / np.pi) * (np.arctan(Base / (2.0 * z)) + (Base * z) / (2.0 * z**2 + 0.5 * Base**2))
+    return u
+
+def uniform(z, Load):
+    u = np.full(z.shape[1], Load, dtype=np.float64)   # load everywhere
+    u[np.isclose(z[0], 0.0)] = 0.0                    # top boundary x=0 -> 0
+    return u
+
+
+
+def Get_Terazaghi1dMultilayer_FEA(depths, num:float, Load:float, T:float, time_steps:int, Cv: list[float], Mv: list[float], Base:float, U0=True):
+
+
+    # defining further paramters 
+    dt = T / (time_steps - 1)
+    H = max(depths)
+    z = np.linspace(0, H, num, dtype= np.float64)# kept as small and not zero to prevent error
+
+
+    def Boussinesq(z):  # no input paramters required as this is only recalled under the same paramter each time
+        """
+        Boussinesq initial condition (strip load, centreline) for Terzaghi consolidation.
+        Assumptions:
+        - uniform embankment pressure q
+        - embankment width B
+        - 1D column at centreline
+        - elastic stress distribution used only for initial condition
+        """
+        z = np.maximum(z,1e-12)
+        u = (2.0 * Load / np.pi) * (np.arctan(Base / (2.0 * z)) + (Base * z) / (2.0 * z**2 + 0.5 * Base**2))
+        return u
+
+    def uniform(z):
+        u = np.full(z.shape[1], Load, dtype=np.float64)   # load everywhere
+        u[np.isclose(z[0], 0.0)] = 0.0                    # top boundary x=0 -> 0
+        return u
+
+
+    # def mesh     
+    msh = mesh.create_interval(
+        comm=MPI.COMM_WORLD,
+        nx=num,
+        points=[0.0, H],
+    )
+
+    # defining dimensions and connectivity 
+    tdim = msh.topology.dim
+    msh.topology.create_connectivity(tdim, 0)
+    conn = msh.topology.connectivity(tdim, 0)
+
+
+    num_cells_local = msh.topology.index_map(tdim).size_local
+    cell_verts = conn.array.reshape(num_cells_local, 2)
+
+    x = msh.geometry.x[:,0] # z geomtry only 
+    midpoints = 0.5 * (x[cell_verts[:, 0]] + x[cell_verts[:, 1]]) # cell_verts the 0 or 1 talks about the left and right respectively
+
+    cell_markers = np.zeros(num_cells_local, dtype=np.int32)
+    z_interface = [0.0] + depths # adding first zero interface 
+
+    DG0 = fem.functionspace(msh, ("DG", 0))
+
+    for i in range(len(Cv)):
+        z0 = z_interface[i]
+        z1 = z_interface[i+1]
+        mask = (midpoints >= z0) & (midpoints < z1)  # returning on true bo0lean
+        cell_markers[mask] = i + 1
+
+    # include bottom endpoint safely in last layer
+    cell_markers[midpoints >= z[-2]] = len(Cv)
+
+
+    kappa = fem.Function(DG0)
+
+    dofmap = DG0.dofmap
+    kappa.x.array[:] = 0.0
+
+    for cell in range(num_cells_local):
+        lid = cell_markers[cell]
+        if lid ==0 :
+            continue
+        dof = dofmap.cell_dofs(cell)[0]
+        kappa.x.array[dof] = Cv[lid - 1]
+
+
+    # check 
+    kappa.x.scatter_forward()
+
+    # THIS IS FOR CHECKING IF MAKES SENSE
+    # for c in range(min(10, num_cells_local)):
+    #    dof = DG0.dofmap.cell_dofs(c)[0]
+    #    print("cell", c, "layer", cell_markers[c], "kappa", kappa.x.array[dof])
+
+    V = fem.functionspace(msh, ("Lagrange", 1))
+
+    # Solution functions
+    u_n = fem.Function(V)
+
+    # Initial condition
+    u_n.interpolate(uniform)
+
+    fdim = msh.topology.dim - 1
+    boundary_facets = mesh.locate_entities_boundary(
+        msh, fdim,
+        marker=lambda x: np.isclose(x[0], 0.0)
+    )
+
+    dofs = fem.locate_dofs_topological(V, fdim, boundary_facets)
+    bc = fem.dirichletbc(PETSc.ScalarType(0), dofs, V)
+
+
+    V = fem.functionspace(msh, ("Lagrange", 1))
+
+    # Solution functions
+    u_n = fem.Function(V)
+
+    initial_condition = lambda x: Boussinesq(x[0])
+
+    # Initial condition
+    u_n.interpolate(uniform)
+
+    fdim = msh.topology.dim - 1
+    boundary_facets = mesh.locate_entities_boundary(
+        msh, fdim,
+        marker=lambda x: np.isclose(x[0], 0.0)
+    )
+
+    dofs = fem.locate_dofs_topological(V, fdim, boundary_facets)
+    bc = fem.dirichletbc(PETSc.ScalarType(0), dofs, V)
+
+    uh = fem.Function(V)
+    uh.name = "uh"
+    uh.interpolate(uniform)
+    # xdmf.write_function(uh,t)
+
+    # varational form
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    # PETSc make sure the background stuff doesnt explode
+    a = (u * v) * ufl.dx + dt * kappa  * ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L = (u_n) * v * ufl.dx
+    bilinear_form = fem.form(a)
+    linear_form = fem.form(L)
+
+    A = assemble_matrix(bilinear_form, bcs = [bc])
+    A.assemble()
+    b = petsc.create_vector(fem.extract_function_spaces(linear_form))
+
+    # creating linear solver 
+    solver = PETSc.KSP().create(msh.comm)
+    solver.setOperators(A)
+    solver.setType(PETSc.KSP.Type.PREONLY)
+    solver.getPC().setType(PETSc.PC.Type.LU)
+
+
+    u_hist = np.zeros((time_steps, uh.x.array.size), dtype=float)
+    u_hist[0, :] = uh.x.array.copy()   # initial state
+    x = V.mesh.geometry.x[:, 0].copy()
+
+
+    for i in range(time_steps - 1):
+        with b.localForm() as loc_b:
+            loc_b.set(0.0)
+        assemble_vector(b, linear_form)
+
+        apply_lifting(b ,[bilinear_form], [[bc]])
+        b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES,
+                    mode=PETSc.ScatterMode.REVERSE)
+        set_bc(b, [bc])
+
+        # Solve
+        solver.solve(b, uh.x.petsc_vec)
+
+        # Update time-step solution
+        u_n.x.array[:] = uh.x.array
+        u_n.x.scatter_forward()
+
+        u_hist[i + 1, :] = uh.x.array.copy()
+
+    # xdmf.close()
+    A.destroy()
+    b.destroy()
+    solver.destroy
+
+    u0 = u_hist[0, :]                 # initial condition in space
+    local_dcons = 1 - u_hist / u0[None,:]
+    local_dcons[:,0] = int(1)
+    
+
+    return local_dcons
+
+
+# Embankment properties
+Load = 100 # load (kPa)
+Base = 10 # embankment width (m)
+ 
+num = 5
+
+name = ["Made ground", "Soft clay", "Firm clay"]
+depths = [1, 2, 4, 5]
+Cv = [2e-7,2e-7, 2e-7, 2e-7] # this is Cv 
+Mv = [5e-4, 10e-4, 5e-4, 5e-4]
+
+T = (60*60*24) * 365 # final time (days)
+time_steps = 10
+
+fem_cdata = Get_Terazaghi1dMultilayer_FEA(depths, num, Load, T, time_steps, Cv,Mv, Base = 10, U0=True)
+
+print(fem_cdata)
